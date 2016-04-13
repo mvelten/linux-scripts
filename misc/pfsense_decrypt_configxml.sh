9e9446e00eb6fe3bf855842ff9f91fc13bc55457
Mathias Velten <mathias.velten@flavia-it.de>
2016-04-13 15:46:13 +0200

    added script to decrypt pfsense config.xml

---
+++ misc/pfsense_decrypt_configxml.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+USAGE="$0 encrypted_config.xml"
+
+if [ $# -ne "1" ]
+then
+	echo $USAGE
+	exit 1
+fi
+
+TMP_FILE=tmp_$1
+
+sed '1d' $1 > $TMP_FILE.base64
+sed -i '$d' $TMP_FILE.base64
+
+base64 -d $TMP_FILE.base64 > $TMP_FILE.enc
+
+openssl enc -d -aes-256-cbc -in $TMP_FILE.enc -out $1_dec.xml
+
+rm $TMP_FILE.*
+
+exit 0
